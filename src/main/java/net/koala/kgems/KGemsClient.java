3c4979899cde31f15bfb8dfe14c58c964567e812
BuildTools <unconfigured@null.spigotmc.org>
2023-12-28 21:50:56 +0000

    important fr

---
+++ src/main/java/net/koala/kgems/KGemsClient.java
@@ -0,0 +1,11 @@
+package net.koala.kgems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KGemsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
