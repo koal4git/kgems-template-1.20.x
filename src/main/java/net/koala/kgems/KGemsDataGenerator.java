0109bc2fec660b808d92a1f8fc9054c1b52e2c49
BuildTools <unconfigured@null.spigotmc.org>
2023-12-28 15:34:07 +0000

    Initial commit

---
+++ src/main/java/net/koala/kgems/KGemsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.koala.kgems;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KGemsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
